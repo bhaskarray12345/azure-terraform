de9c945d727a106951d00438c63dc0cd5ebbfb84
Bhaskar Ray <bhaskar.61094746@ltimindtree.com>
2023-12-04 14:13:32 +0530

    with azure service principal

--- environments/prod/versions.tf
+++ environments/prod/versions.tf
@@ -4,6 +4,7 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.75.0"
       
+      
     #   configuration_aliases = [
     #     azurerm.vhub
     #   ]
@@ -26,8 +27,25 @@ terraform {
     
   }
   required_version = ">= 1.3.5"
+
 }
+
+
 
 provider "azurerm" {
   features {}
+
+  skip_provider_registration = "true"
+  
+  #azure terraform
+  # client_id       = "b9595cda-23f0-4a07-9a09-9b1dcb803340"
+  # client_secret   = "XH~8Q~x6rdTuIGPkAqF.OTdDE0jcDh.aj1wJtcnx"
+  # tenant_id       = "c5b71890-2994-4268-8eed-c824c357f508"
+  # subscription_id = "767f3270-a4e3-4998-8a32-00877bf77174"
+
+#bhaskarray0820210080-iothubnxtdev-767f3270-a4e3-4998-8a32-00877bf77174
+  client_id       = "a2f5c48e-400c-49e9-b154-9ac4a9cb1b37"
+  client_secret   = "AZU8Q~UJlht13lIQ5aCyf6G1ED0vK6JGX40Ixa2j"
+  tenant_id       = "c5b71890-2994-4268-8eed-c824c357f508"
+  subscription_id = "767f3270-a4e3-4998-8a32-00877bf77174"
 }
